a1bc3d0628b42d351b2d62bd4f65c04349352f30
Daniela Valencia <daniela.valenica@gmail.com>
2019-03-25 22:10:45 +0000

    Day one

--- js/script.js
+++ js/script.js
@@ -3,9 +3,20 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
+var gif_url = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC"
+
+
 $("#search-button").click(function(){
-  
-  
-  
+  $.ajax({
+    url: gif_url,
+    method: "GET",
+    success: function(response) {
+    
+    
+        //console.log(response.data);
+        
+        $("body").append("<img src=" + reponse.data.images.fixed_height_still.url + ">");
+    }  
+  });
 });
 
